f2a16db562aa9a62ca9b73a2146d435ad2dde118
jbroberg42 <ninjonah1@gmail.com>
2026-05-02 11:10:42 -0500

    snowmen movement
    - renamed RayCastRight to RayCastBottomRight etc.
    - Created RayCastRight and Left to instead detect walls

--- scripts/snowman.gd
+++ scripts/snowman.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-const speed = 60
+@onready var game_manager: Node = %GameManager
 
+const speed = 60
 var direction = 1
 #var carrot = preload("res://scenes/carrot.tscn")
 var is_carrot
@@ -9,10 +10,15 @@ var is_carrot
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_bottom_right: RayCast2D = $RayCastBottomRight
+@onready var ray_cast_bottom_left: RayCast2D = $RayCastBottomLeft
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _ready():
 	is_carrot = false
+	#connect signals with code.  Otherwise you have to connect each mob manually.  Which sucks.
+	game_manager.spring.connect(_on_game_manager_spring)
+	game_manager.winter.connect(_on_game_manager_winter)
 
 func _process(delta):
 #	if Input.is_action_just_pressed("switch"):
@@ -28,12 +34,13 @@ func _process(delta):
 			#animated_sprite_2d.play("resurrects")
 			#timer.start() #the timer below will trigger and turn the snowman hostile again
 		
-	#move side to side, not off cliff
+	#move side to side, not off cliff or through walls
+	#RayCastRight and RayCastLeft detect walls, the Bottom ones detect cliffs
 	if !is_carrot:
-		if not ray_cast_right.is_colliding():
+		if ray_cast_right.is_colliding() || not ray_cast_bottom_right.is_colliding():
 			direction = -1
 			animated_sprite_2d.flip_h = false
-		if not ray_cast_left.is_colliding():
+		if ray_cast_left.is_colliding() || not ray_cast_bottom_left.is_colliding():
 			direction = 1
 			animated_sprite_2d.flip_h = true
 		position.x += direction * speed * delta
