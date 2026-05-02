754b65d967007dd5ab6919a4b4e830fd3c95c884
parkerbroberg <parkerbroberg01@gmail.com>
2026-05-02 03:13:00 -0500

    implemented evil flower platform

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -1,7 +1,36 @@
 extends Node2D
 @onready var anim = $AnimatedSprite2D
+@onready var timer = $Area2D/Timer
+@onready var timer_2 = $Area2D/Timer2
+@onready var timer_3 = $Area2D/Timer3
+@onready var collision_shape_2d = $StaticBody2D/CollisionShape2D
+
+var readyattack = true
+
+func _ready():
+	$killzone/CollisionShape2D.disabled = true
+	
+
+func _on_area_2d_body_entered(body):
+	if readyattack == true:
+		readyattack = false
+		anim.play("attack")
+		timer.start()
+	
+	
+func _on_timer_timeout():
+	$killzone/CollisionShape2D.disabled = false
+	collision_shape_2d.disabled = true
+	timer_2.start()
+
+
+func _on_timer_2_timeout():
+	$killzone/CollisionShape2D.disabled = true
+	timer_3.start()
 	
 
 
-func _on_area_2d_body_entered(body):
-	anim.play("attack")
+func _on_timer_3_timeout():
+	collision_shape_2d.disabled = false
+	anim.play("idle")
+	readyattack = true
