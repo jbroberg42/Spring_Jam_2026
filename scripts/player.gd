c0023e8b7265bbb0b2a4ba286427a4baebe8381c
jbroberg42 <ninjonah1@gmail.com>
2026-05-01 23:00:17 -0500

    working on tilemapping

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,6 +18,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.play("jump")
+		$AnimatedSprite2D.play("idle")
+
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
