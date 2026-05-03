c298d190cfe8eb7c3724abfed6741def0be3722d
jbroberg42 <ninjonah1@gmail.com>
2026-05-02 20:34:53 -0500

    struggling with season sensitive tilemap

---
+++ scripts/season_sensitive_tilemap.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var game_manager: Node = %GameManager
+
+const SEASON_TEXTURES = [
+	"res://assets/sprites/WinterTileSet.png",
+	"res://assets/sprites/SpringTileSet.png"
+]
+
+func _ready() -> void:
+	game_manager.spring.connect(_on_game_manager_spring)
+	game_manager.winter.connect(_on_game_manager_winter)
+	if GameState.is_winter:
+		_on_game_manager_winter()
+	else:
+		_on_game_manager_spring()
+
+
+
+func _on_game_manager_winter() -> void:
+	$".".tile_set.get_source(1).texture = preload("res://assets/sprites/WinterTileSet.png")
+func _on_game_manager_spring() -> void:
+	$".".tile_set.tile_set_texture = SEASON_TEXTURES[1]
