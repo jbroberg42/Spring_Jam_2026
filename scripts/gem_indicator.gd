54ba8ca020764ed921796a6344eefd4552feea61
jbroberg42 <ninjonah1@gmail.com>
2026-05-03 04:20:16 -0500

    stuff

--- scripts/gem_indicator.gd
+++ scripts/gem_indicator.gd
@@ -4,7 +4,7 @@ var colors: Array[String] = GameState.LEVEL_COLORS
 
 func _process(delta: float) -> void:
 	if GameState.have_macguffin:
-		$AnimatedSprite2D.play(colors[GameState.level])
+		$AnimatedSprite2D.play(colors[GameState.level - 1])
 		$AnimatedSprite2D.show()
 	else:
 		$AnimatedSprite2D.hide()
