89a048b5de2fccb80ad14bb70aae65231ab4185a
jbroberg42 <ninjonah1@gmail.com>
2026-05-03 00:12:28 -0500

    snow piles, working on macguffin indicator

---
+++ scripts/gem_indicator.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var game_manager: Node = %GameManager
+
+func _on_ready():
+	$Label.text = ""
+
+
+func _on_game_manager_spring() -> void:
+	$AnimatedSprite2D.play("spring")
+	cooldown_label()
+func _on_game_manager_winter() -> void:
+	$AnimatedSprite2D.play("winter")
+	cooldown_label()
+	
+func cooldown_label():
+	var a = GameState.ABILITY_COOLDOWN
+	for i in a:
+		$Label.text = str(a)
+		a -= 1
+		await get_tree().create_timer(1).timeout
+	$Label.text = ""
+	
--- scripts/macguffin.gd
+++ scripts/macguffin.gd
@@ -2,8 +2,18 @@ extends Area2D
 
 @onready var game_manager: Node = %GameManager
 
-
+func _ready():
+	game_manager.spring.connect(_on_game_manager_spring)
+	game_manager.winter.connect(_on_game_manager_winter)
 
 func _on_body_entered(body: Node2D) -> void:
 	GameState.have_macguffin = true
 	queue_free()
+
+func _on_game_manager_spring() -> void:
+	show()
+	$CollisionShape2D.disabled = false
+	
+func _on_game_manager_winter() -> void:
+	hide()
+	$CollisionShape2D.disabled = true
