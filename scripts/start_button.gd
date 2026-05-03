0d07d4fc0a1b60ef21a24675cc633787515b4be8
jbroberg42 <ninjonah1@gmail.com>
2026-05-03 01:10:39 -0500

    remove glitched scene

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -3,5 +3,5 @@ extends Button
 
 func _on_pressed() -> void:
 	print("hello")
-	get_tree().change_scene_to_file(GameState.LEVEL_LIST[GameState.level + 1])
+	get_tree().change_scene_to_file(GameState.LEVEL_LIST[GameState.level])
 	GameState.level += 1
